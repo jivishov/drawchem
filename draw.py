c692c5c4b81c1cb6f1fcbb4f16ebd776eba8d1b6
jivishov <97884055+jivishov@users.noreply.github.com>
2023-11-17 12:38:29 -0600

    Update draw.py

--- draw.py
+++ draw.py
@@ -1,22 +1,31 @@
 import streamlit as st
+import pubchempy as pcp
 from rdkit import Chem
 from rdkit.Chem import Draw
+
+def name_to_smiles(name):
+    try:
+        compound = pcp.get_compounds(name, 'name')[0]
+        return compound.isomeric_smiles
+    except:
+        return None
 
 def draw_molecule(smiles):
     molecule = Chem.MolFromSmiles(smiles)
     return Draw.MolToImage(molecule)
 
 def main():
-    st.title('Chemical Structure Drawer')
+    st.title('Chemical Structure Drawer from Name')
 
-    smiles_string = st.text_input("Enter a SMILES string:", "")
-    
-    if smiles_string:
-        try:
+    chemical_name = st.text_input("Enter a chemical name:", "")
+
+    if chemical_name:
+        smiles_string = name_to_smiles(chemical_name)
+        if smiles_string:
             image = draw_molecule(smiles_string)
             st.image(image)
-        except:
-            st.error("Invalid SMILES string. Please try again.")
+        else:
+            st.error("Could not find the chemical. Please try a different name.")
 
 if __name__ == "__main__":
     main()
