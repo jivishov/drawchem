47dde23562f4fdf64519e1bdafcee676d7ef735a
jivishov <97884055+jivishov@users.noreply.github.com>
2023-11-17 10:42:30 -0600

    Update draw.py

--- draw.py
+++ draw.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pubchempy as pcp
-import requests
-from PIL import Image
-from io import BytesIO
+from rdkit import Chem
+from rdkit.Chem import Draw
 
 def name_to_smiles(name):
     try:
@@ -11,13 +10,9 @@ def name_to_smiles(name):
     except:
         return None
 
-def smiles_to_image(smiles, width=300, height=300):
-    url = f"https://cactus.nci.nih.gov/chemical/structure/{smiles}/image"
-    params = {'width': width, 'height': height}
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        return Image.open(BytesIO(response.content))
-    return None
+def draw_molecule(smiles):
+    molecule = Chem.MolFromSmiles(smiles)
+    return Draw.MolToImage(molecule)
 
 def main():
     st.title('Chemical Structure Drawer from Name')
@@ -27,11 +22,8 @@ def main():
     if chemical_name:
         smiles_string = name_to_smiles(chemical_name)
         if smiles_string:
-            image = smiles_to_image(smiles_string)
-            if image:
-                st.image(image, use_column_width=True)
-            else:
-                st.error("Could not generate the image for this chemical.")
+            image = draw_molecule(smiles_string)
+            st.image(image)
         else:
             st.error("Could not find the chemical. Please try a different name.")
 
